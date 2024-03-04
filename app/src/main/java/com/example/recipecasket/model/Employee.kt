f7421808347ccd505017858af36f411f55b8c481
RomanRedkovskiy <romanredkovskiy10@gmail.com>
2024-03-04 14:05:01 +0300

    Initial commit

---
+++ app/src/main/java/com/example/recipecasket/model/Employee.kt
@@ -0,0 +1,8 @@
+package com.example.recipecasket.model
+
+data class Employee(
+    var empId: String? = null,
+    var empName: String? = null,
+    var empAge: String? = null,
+    var empSalary: String? = null
+)
