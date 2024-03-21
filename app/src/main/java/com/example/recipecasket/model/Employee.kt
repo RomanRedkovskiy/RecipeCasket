bb8cf5fa70ffc107317a3753d93a893dd98879d7
RomanRedkovskiy <92377542+RomanRedkovskiy@users.noreply.github.com>
2024-03-21 18:34:57 +0300

    Delete app/src/main/java/com/example/recipecasket/model/Employee.kt

--- app/src/main/java/com/example/recipecasket/model/Employee.kt
+++
@@ -1,8 +0,0 @@
-package com.example.recipecasket.model
-
-data class Employee(
-    var empId: String? = null,
-    var empName: String? = null,
-    var empAge: String? = null,
-    var empSalary: String? = null
-)
